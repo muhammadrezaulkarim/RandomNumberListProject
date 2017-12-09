76a0eec040772826d8198d5bcc4b79fb7409c38c
muhammadrezaulkarim <r_karimcs@yahoo.com>
2017-12-08 18:49:10 -0700

    project initially added

---
+++ RandomNumberListProject/RandomMinMaxNumberList.cs
@@ -0,0 +1,165 @@
+﻿/* Author: Muhammad Rezaul Karim
+ * Date: December 07, 2017
+ */
+using System;
+using System.Collections;
+
+namespace RandomNumberListProject
+{
+    public class RandomMinMaxNumberList
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.Write("Please enter lower limit (press 'enter' to use the default value 1):");
+            string input = Console.ReadLine();
+
+            int lowerLimit = 0;
+          //read lower limit
+            if (!input.Trim().Equals(""))
+                lowerLimit = int.Parse(input);   // if user entered a number, parse it
+            else
+                lowerLimit = 1; // if user did not enter a number, set lower limit to the default value
+
+            Console.Write("Please enter upper limit (press 'enter' to use the default value 10000):");
+            input = Console.ReadLine();
+            
+            //read upper limit
+            int upperLimit = 0;
+            if (!input.Trim().Equals(""))
+               upperLimit = int.Parse(input);  // if user entered a number, parse it
+            else
+               upperLimit = 10000; // if user did not enter a number, set upper limit to the default value
+
+            Console.Write("Please enter total numbers to be generated (press 'enter' to use the default value 10000):");
+            //read total numbers to be generated
+            input = Console.ReadLine();
+            int totalNumbers = 0;
+            if (!input.Trim().Equals(""))   // if user entered a number, parse it
+                totalNumbers = int.Parse(input);
+            else
+                totalNumbers = 10000;  // if user did not enter a number, set total numbers to the default value
+
+            Console.WriteLine();
+            //check whether upperLimit greater than lowerLimit
+            if (lowerLimit >= upperLimit)
+            {
+                Console.WriteLine("Error: Lower limit must be less than upper limit.");
+                Console.WriteLine("Press any key to quit...");
+                Console.ReadKey();
+                return;
+            }
+
+            //check whether total numbers less than equal to upper limit
+            if (totalNumbers > upperLimit)
+            {
+                Console.WriteLine("Error: Total numbers must be less than or equal to upperLimit.");
+                Console.WriteLine("Press any key to quit...");
+                Console.ReadKey();
+                return;
+            }
+
+            int[] finalNumberArray = null;
+            // keep track when the number generation started
+            DateTime startTime = DateTime.Now;
+           finalNumberArray=GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+          // finalNumberArray=GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+
+            // keep track when the number generation finished. 
+            DateTime endTime = DateTime.Now;
+            TimeSpan elapsedTime = endTime.Subtract(startTime);
+
+            printListOfNumbers(finalNumberArray);
+
+            Console.WriteLine();
+            Console.WriteLine("Time taken to generate (in seconds):" + elapsedTime.TotalSeconds);
+
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
+
+      public static int [] GenerateRandomListHashTableBased(int lowerLimit, int upperLimit, int totalNumbers)
+        {
+            // store generated numbers
+            Hashtable alreadyGeneratedNumbers = new Hashtable();
+            //using a time-dependent default seed value to produce different sequences of random numbers
+            Random rnd = new Random();
+            int[] finalNumberArray = new int[totalNumbers];
+            int tempNum = -1;
+
+            for (int count = 0; count < totalNumbers;)
+            {
+                //generate a random number between the lower and upper limit (inclusive)
+                tempNum = rnd.Next(lowerLimit, upperLimit + 1);
+
+                // If already not generated, consider the generated number for storing
+                if (!alreadyGeneratedNumbers.Contains(tempNum))
+                {
+                    // store in the hash table
+                    alreadyGeneratedNumbers.Add(tempNum, 1);
+                    // store in the static array
+                    finalNumberArray[count] = tempNum;
+                    count++;
+                }
+
+            }
+
+            return finalNumberArray;
+         
+
+        }
+        
+     /*  public static int [] GenerateRandomListArrayListBased(int minLimit, int maxLimit, int totalNumbers)
+        {
+   
+            ArrayList alreadyGeneratedNumbers = new ArrayList();
+            //using a time-dependent default seed value to produce different sequences of random numbers
+            Random rnd = new Random();
+
+            int tempNum = -1;
+            int[] finalNumberArray = new int[totalNumbers];
+
+            for (int count = 0; count < totalNumbers;)
+            {
+                //generate a random number between the lower and upper limit (inclusive)
+                tempNum = rnd.Next(minLimit, maxLimit + 1);
+                
+                // If already not generated, consider the generated number for storing
+                if (!alreadyGeneratedNumbers.Contains(tempNum))
+                {
+                    // store in a array list object
+                    alreadyGeneratedNumbers.Add(tempNum);
+                    // store in a static array
+                    finalNumberArray[count] = tempNum;
+                    count++;
+                }
+
+            }
+
+            return finalNumberArray; 
+
+        } */
+
+        public static void printListOfNumbers(int[] finalNumberArray)
+        {
+            for (int i = 0; i < finalNumberArray.Length; i++)
+            {
+                Console.Write(finalNumberArray[i] + " ");
+                // display 15 numbers per line
+                if (i>0 && (i % 15 == 0))
+                    Console.WriteLine();
+            }
+
+            Array.Sort(finalNumberArray); // sort the numbers in ascending order
+            Console.WriteLine();
+            Console.WriteLine();
+            // Check the output
+            //Minimum among the generated numbers
+            Console.WriteLine("Minimum value(among generated numbers): " + finalNumberArray[0]);
+            //Maximum among the generated numbers
+            Console.WriteLine("Maximum value(among generated numbers): " + finalNumberArray[finalNumberArray.Length-1]);
+            //Total generated numbers
+            Console.Write("Total generated numbers: " + finalNumberArray.Length);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest.cs
@@ -0,0 +1,159 @@
+﻿/* Author: Muhammad Rezaul Karim
+ * Date: December 07, 2017
+ */
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomNumberListProject;
+using System.Collections;
+
+namespace UnitTestProject
+{
+
+    [TestClass]
+    public class UnitTest
+    {
+        int lowerLimit;
+        int upperLimit;
+        int totalNumbers;
+
+        // This method will initialize the required variables (e.g. lower limit, upper limit and total numbers)
+        [TestInitialize]
+        public void InitializeVariablesBeforeTest()
+        {
+            lowerLimit = 1;
+            upperLimit = 10000;
+            totalNumbers = 10000; 
+        }
+
+        // This method checks whether each number in the generated list is unique and between lower and upper limit (inclusive)
+        [TestMethod]
+        public void TestMethodGenerateRandomListHashTableBasedUniquenessAndTotalNumberCountTest()
+        {
+
+            // generate all possible numbers between lower limit and upper limit
+            int[] possibleRangeOfNumbers = new int[upperLimit-lowerLimit + 1];
+            for (int num = lowerLimit, index = 0; num <= upperLimit; num++, index++)
+                possibleRangeOfNumbers[index] = num;
+
+            // run once
+            int[] generatedNumbers = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+
+            //check whether all generated numbers are unique
+            CollectionAssert.AllItemsAreUnique(generatedNumbers);
+            
+            //check whether all generated numbers are within the lower and upper limit range
+            CollectionAssert.IsSubsetOf(generatedNumbers, possibleRangeOfNumbers);
+
+            // check whether the generated list have expected N (e.g. totalNumbers = 10000) numbers
+            Assert.AreEqual(generatedNumbers.Length, totalNumbers);
+
+            // run once again
+            generatedNumbers = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+
+            //check whether all generated numbers are unique
+            CollectionAssert.AllItemsAreUnique(generatedNumbers);
+
+            //check whether all generated numbers are within the lower and upper limit range
+            CollectionAssert.IsSubsetOf(generatedNumbers, possibleRangeOfNumbers);
+
+            // check whether the generated list have expected N (e.g. totalNumbers = 10000) numbers
+            Assert.AreEqual(generatedNumbers.Length, totalNumbers);
+
+        }
+
+        // This method checks whether a list of N (e.g. totalNumbers = 10000) numbers is generated in random order
+        [TestMethod]
+        public void TestMethodGenerateRandomListHashTableBasedRandomDifferentOrderTest()
+        {
+            // Check whether generated list of numbers are in different order or not. Test fails if not different
+            int[] numberArray1 = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+            int[] numberArray2 = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+            CollectionAssert.AreNotEqual(numberArray1, numberArray2);
+
+            // Check whether generated list of numbers are in different order or not. Test fails if not different
+            numberArray1 = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+            numberArray2 = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+            CollectionAssert.AreNotEqual(numberArray1, numberArray2);
+
+            // Check whether generated list of numbers are in different order or not. Test fails if not different
+            numberArray1 = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+            numberArray2 = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+            CollectionAssert.AreNotEqual(numberArray1, numberArray2);
+
+            // Check whether generated list of numbers are in different order or not. Test fails if not different
+            numberArray1 = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+            numberArray2 = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+            CollectionAssert.AreNotEqual(numberArray1, numberArray2);
+
+            // Check whether generated list of numbers are in different order or not. Test fails if not different
+            numberArray1 = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+            numberArray2 = RandomMinMaxNumberList.GenerateRandomListHashTableBased(lowerLimit, upperLimit, totalNumbers);
+            CollectionAssert.AreNotEqual(numberArray1, numberArray2);
+        }
+        /*
+                // This method checks whether each number in the generated list is unique and between lower and upper limit (inclusive)
+                [TestMethod]
+                public void TestMethodGenerateRandomListArrayListBasedUniquenessAndTotalNumberCountTest()
+                {
+                    // generate all possible numbers between lower limit and upper limit
+                    int[] possibleRangeOfNumbers = new int[upperLimit - lowerLimit + 1];
+                    for (int num = lowerLimit, index = 0; num <= upperLimit; num++, index++)
+                        possibleRangeOfNumbers[index] = num;
+
+                    // run once
+                    int[] generatedNumbers = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+
+                    //check whether all generated numbers are unique
+                    CollectionAssert.AllItemsAreUnique(generatedNumbers);
+
+                    //check whether all generated numbers are within the lower and upper limit range
+                    CollectionAssert.IsSubsetOf(generatedNumbers, possibleRangeOfNumbers);
+
+                    // check whether the generated list have expected N (e.g. totalNumbers = 10000) numbers
+                    Assert.AreEqual(generatedNumbers.Length, totalNumbers);
+
+                    // run once again
+                    generatedNumbers = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+
+                    //check whether all generated numbers are unique
+                    CollectionAssert.AllItemsAreUnique(generatedNumbers);
+
+                    //check whether all generated numbers are within the lower and upper limit range
+                    CollectionAssert.IsSubsetOf(generatedNumbers, possibleRangeOfNumbers);
+
+                    // check whether the generated list have expected N (e.g. totalNumbers = 10000) numbers
+                    Assert.AreEqual(generatedNumbers.Length, totalNumbers);
+                }
+
+                // This method checks whether a list of N (e.g. totalNumbers = 10000) numbers is generated in random order
+                [TestMethod]
+                public void TestMethodGenerateRandomListArrayListBasedRandomDifferentOrderTest()
+                {
+                    // Check whether generated list of numbers are in different order or not. Test fails if not different
+                    int[] numberArray1 = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+                    int[] numberArray2 = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+                    CollectionAssert.AreNotEqual(numberArray1, numberArray2);
+
+                    // Check whether generated list of numbers are in different order or not. Test fails if not different
+                    numberArray1 = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+                    numberArray2 = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+                    CollectionAssert.AreNotEqual(numberArray1, numberArray2);
+
+                    // Check whether generated list of numbers are in different order or not. Test fails if not different
+                    numberArray1 = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+                    numberArray2 = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+                    CollectionAssert.AreNotEqual(numberArray1, numberArray2);
+
+                    // Check whether generated list of numbers are in different order or not. Test fails if not different
+                    numberArray1 = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+                    numberArray2 = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+                    CollectionAssert.AreNotEqual(numberArray1, numberArray2);
+
+                    // Check whether generated list of numbers are in different order or not. Test fails if not different
+                    numberArray1 = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+                    numberArray2 = RandomMinMaxNumberList.GenerateRandomListArrayListBased(lowerLimit, upperLimit, totalNumbers);
+                    CollectionAssert.AreNotEqual(numberArray1, numberArray2);
+                } */
+    }
+}
